0f0701a387f9d9c879c4b60a3cb9f5d71a6b3424
Aidan <walkeraidan94@gmail.com>
2021-10-18 23:43:52 +0100

    folder renamed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import { store } from "./redux/store/store";
+import { store } from "./features/store/store";
 
 ReactDOM.render(
   <Provider store={store}>
